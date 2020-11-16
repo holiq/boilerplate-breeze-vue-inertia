bddc9829a1ffca78281b13edd03c10cf11ba16fe
holiq <holiq.ibrahim376@gmail.com>
2020-11-16 17:49:55 +0700

    remove blade

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
 require('./bootstrap');
+
+import { App, plugin } from '@inertiajs/inertia-vue'
+import VueMeta from 'vue-meta'
+import Vue from 'vue';
+
+Vue.mixin({ methods: { route } });
+Vue.use(plugin)
+Vue.use(VueMeta)
+
+const el = document.getElementById('app');
+
+new Vue({
+    render: h => h(App, {
+        props: {
+            initialPage: JSON.parse(el.dataset.page),
+            resolveComponent: name => require(`./Pages/${name}`).default,
+        },
+    }),
+}).$mount(el)
+
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,9 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
-    ]);
+    ])
+    .webpackConfig(resolve: {
+        alias: {
+            '@': path.resolve('resources/js'),
+        },
+    });
