97e79a9c77fb6f06cf3efa904fa6a345ceb7cc36
holiq <holiq.ibrahim376@gmail.com>
2020-11-23 01:05:17 +0700

    add props user

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -38,6 +38,9 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'status' => fn () => $request->session()->get('status'),
+            'user' => fn () => $request->user()
+                ? $request->user()->only('id', 'name', 'email')
+                : null,
             'errors' => fn () => $request->session()->get('errors')
                 ? $request->session()->get('errors')->getBag('default')->getMessages()
                 : (object) []
--- resources/js/Layouts/Navigation.vue
+++ resources/js/Layouts/Navigation.vue
@@ -1,128 +1,118 @@
 <template>
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
-                    <inertia-link :href="route('dashboard')">
-                        <AplicationLogo class="block h-10 w-auto fill-current text-gray-600" />
-                    </inertia-link>
+    <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+        <!-- Primary Navigation Menu -->
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="flex-shrink-0 flex items-center">
+                        <inertia-link :href="route('dashboard')">
+                            <AplicationLogo class="block h-10 w-auto fill-current text-gray-600" />
+                        </inertia-link>
+                    </div>
+                    <!-- Navigation Links -->
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                            Dashboard
+                        </NavLink>
+                    </div>
                 </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                        Dashboard
-                    </NavLink>
+                <!-- Settings Dropdown -->
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <Dropdown align="right" width="48">
+                        <template v-slot:trigger>
+                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <div>name</div>
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </template>
+                        <template v-slot:content>
+                            <!-- Authentication -->
+                            <form @submit.prevent="logout">
+                                <DropdownLink as="button">
+                                    Logout
+                                </DropdownLink>
+                            </form>
+                        </template>
+                    </Dropdown>
                 </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <Dropdown align="right" width="48">
-                    <template v-slot:trigger>
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>name</div>
-
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </template>
-
-                    <template v-slot:content>
-                        <!-- Authentication -->
-                        <form @submit.prevent="logout">
-                            <DropdownLink as="button">
-                                Logout
-                            </DropdownLink>
-                        </form>
-                    </template>
-                </Dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="showingNavigationDropdown = ! showingNavigationDropdown" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                   <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                       <path :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                       <path :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                   </svg>
-                </button>
+                <!-- Hamburger -->
+                <div class="-mr-2 flex items-center sm:hidden">
+                    <button @click="showingNavigationDropdown = ! showingNavigationDropdown" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                            <path :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                Dashboard
-            </ResponsiveNavLink>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="flex items-center px-4">
-                <div class="flex-shrink-0">
-                    <svg class="h-10 w-10 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                    </svg>
+        <!-- Responsive Navigation Menu -->
+        <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
+            <div class="pt-2 pb-3 space-y-1">
+                <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                    Dashboard
+                </ResponsiveNavLink>
+            </div>
+            <!-- Responsive Settings Options -->
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                <div class="flex items-center px-4">
+                    <div class="flex-shrink-0">
+                        <svg class="h-10 w-10 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                    </div>
+                    <div class="ml-3">
+                        <div class="font-medium text-base text-gray-800">{{ $page.props.user.name }}</div>
+                        <div class="font-medium text-sm text-gray-500">{{ $page.props.user.email }}</div>
+                    </div>
                 </div>
-
-                <div class="ml-3">
-                    <div class="font-medium text-base text-gray-800">name</div>
-                    <div class="font-medium text-sm text-gray-500">email</div>
+                <div class="mt-3 space-y-1">
+                    <!-- Authentication -->
+                    <form @submit.prevent="logout">
+                        <ResponsiveNavLink as="button">
+                            Logout
+                        </ResponsiveNavLink>
+                    </form>
                 </div>
             </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Authentication -->
-                <form @submit.prevent="logout">
-                    <ResponsiveNavLink as="button">
-                        Logout
-                    </ResponsiveNavLink>
-                </form>
-            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 </template>
-
 <script>
-import { Inertia } from '@inertiajs/inertia'
-
-import AplicationLogo from '@/Components/AplicationLogo'
-import NavLink from '@/Components/NavLink'
-import Dropdown from '@/Components/Dropdown'
-import DropdownLink from '@/Components/DropdownLink'
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink'
-
-export default {
-    components: {
-        AplicationLogo,
-        NavLink,
-        Dropdown,
-        DropdownLink,
-        ResponsiveNavLink,
-    },
-    data() {
-            return {
-                showingNavigationDropdown: false,
-            }
+    import { Inertia } from '@inertiajs/inertia'
+    
+    import AplicationLogo from '@/Components/AplicationLogo'
+    import NavLink from '@/Components/NavLink'
+    import Dropdown from '@/Components/Dropdown'
+    import DropdownLink from '@/Components/DropdownLink'
+    import ResponsiveNavLink from '@/Components/ResponsiveNavLink'
+    
+    export default {
+        components: {
+            AplicationLogo,
+            NavLink,
+            Dropdown,
+            DropdownLink,
+            ResponsiveNavLink,
         },
-    methods: {
-        logout() {
-            Inertia.post('/logout', this.form, {
-                onSuccess: () => {
-                    window.location.href = "/";
+        data() {
+                return {
+                    showingNavigationDropdown: false,
                 }
-            });
-        },
+            },
+        methods: {
+            logout() {
+                Inertia.post('/logout', this.form, {
+                    onSuccess: () => {
+                        window.location.href = "/";
+                    }
+                });
+            },
+        }
     }
-}
 </script>
